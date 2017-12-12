e4d5e1984ff790d981e64da84f972706153d376e
Gonéri Le Bouder <goneri@redhat.com>
2017-12-12 11:19:29 -0500

    show up the name of the final tarball

--- dci_get_last_component.sh
+++ dci_get_last_component.sh
@@ -7,13 +7,17 @@ fi
 topic_name=$1
 topic_id=$(dcictl --format json topic-list --where name:OSP12|jq -r '.topics[0].id')
 component_id=$(dcictl --format json component-list --topic-id e33fe610-f7ac-4bb5-9c37-423b3ffdd245 --limit 1|jq -r .components[0].id)
-component_name=$(dcictl --format json component-list --topic-id e33fe610-f7ac-4bb5-9c37-423b3ffdd245 --limit 1|jq -r .components[0].name)
+component_name=$(dcictl --format json component-list --topic-id e33fe610-f7ac-4bb5-9c37-423b3ffdd245 --limit 1|jq -r .components[0].name|sed 's, ,_,g')
 component_file_id=$(dcictl --format json component-file-list $component_id|jq -r .component_files[0].id)
+target="~/tmp/dci/${component_name}.tar"
+dir=$(dirname $(echo $target))
 
 echo "topic_id $topic_id"
 echo "component_name $component_name"
 echo "component_id $component_id"
 echo "component_file_id $component_file_id"
-mkdir ~/tmp
-dcictl component-file-download $component_id --file-id $component_file_id --target ~/tmp/toto.tar
+
+[ -d ${dir} ] || mkdir -p ${dir}
+echo "Downloading ${target}"
+dcictl component-file-download $component_id --file-id $component_file_id --target ${target}
 #tar tf ~/tmp/toto.tar|grep container_images.yaml
